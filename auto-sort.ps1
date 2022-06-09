c8d4bf51f2404749bda8713b979c0f300e446289
francovdw <36730526+francovdw@users.noreply.github.com>
2022-06-09 15:54:06 +0200

    Add files via upload

---
+++ auto-sort.ps1
@@ -0,0 +1,57 @@
+# Force User Access Control for script
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# Archive folder location
+$path = "$env:USERPROFILE\Documents\_Archive"
+
+# Start Transcript
+Write-Host ""
+Start-Transcript -path "$path\trans.txt" -Append
+Write-Host ""
+Write-Host ""
+
+# Create folders based on the extension list
+$confirmfolder = Test-Path -PathType Container "$path\_Other"
+    if ($confirmfolder -ne $true){New-Item "$path\_Other" -ItemType Directory -Force -EA SilentlyContinue}
+
+# File name extension list (File types that will be moved to the archived folder)
+$extlist = "*.ps1","*.txt","*.pdf","*.docx","*.doc","*.xlsx","*.xls","*.png","*.jpg","*.csv","*.exe","*.msi","*.zip","*.rar","*.tar","*.log","*.cfg","*.iso","*.rdp","*.mp4","*.mp3","*.wav","*.msg","*.cer","*.deb","*.bat","*.pfx","*.img","*.ini","*.xlsm","*.pptx","*.msu","*.tbxml","*.pcapn","*.drawio"
+
+# File name extension Exclude list (Files to be excluded from being moved)
+# PS: Add Name & File name extension
+$exclude = "auto-sort.lnk","auto-sort.ps1"
+
+# Script
+foreach($extension in $Extension_List)
+    {
+        # Create folder name from extension
+        $folder = $extension.TrimStart("*.")
+        $folder = $folder.ToUpper()
+
+        # Create folder for the file type
+        $ConfirmFolder = Test-Path -PathType Container "$path\$folder" -Verbose
+            if ($confirmfolder -ne $true){New-Item "$path\$folder" -ItemType Directory -Force -EA SilentlyContinue -Verbose}
+
+        # Get files from Downloads & Desktop
+        $Downloads = "$env:USERPROFILE\Downloads\$extension"
+        $Desktop = "$env:USERPROFILE\Desktop\$extension"
+
+        # Move file to folder
+        Move-Item -Path $pDown -Destination "$path\$folder" -Exclude $exclude -Force -Verbose -EA SilentlyContinue
+        Move-Item -Path $pDesk -Destination "$path\$folder" -Exclude $exclude -Force -Verbose -EA SilentlyContinue
+
+            }
+
+# Move all unspecified files & folders into one folder
+Move-Item -Path "$env:USERPROFILE\Downloads\*" -Destination "$Path\_Other" -Exclude $exclude -Force -Verbose -EA SilentlyContinue
+Move-Item -Path "$env:USERPROFILE\Desktop\*" -Destination "$Path\_Other" -Exclude $exclude -Force -Verbose -EA SilentlyContinue
+
+# Stop Transcript
+Write-Host ""
+Write-Host ""
+Stop-Transcript
+Write-Host ""
+Write-Host ""
+Write-Host ""
+pause
+exit
